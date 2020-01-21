207c758ec8eb7e414910126b67925aa57184014b
Lucas Custódio <48959068+lucascust@users.noreply.github.com>
2020-01-20 22:09:18 -0300

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -57,7 +57,13 @@ while (i <= 20) {
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 console.log( 'Números ímpares entre 10 e 20:' );
-// ?
+console.log( 'Números pares entre 10 e 20:' );
+var i = 10;
+while (i <= 20) {
+    i % 2 ? console.log(i) : '';  
+    i++;
+};
+
 
 /*
 Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
@@ -66,7 +72,12 @@ Só vamos mudar o range:
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
 console.log( 'Números pares entre 100 e 120:' );
-// ?
+for(var i=100; i<=200; i+=2){
+    console.log(i);  
+}
 
 console.log( 'Números ímpares entre 111 e 125:' );
-// ?
+for(var i=111; i<=125; i++){
+    i % 2 ? console.log(i) : '';
+    
+}
